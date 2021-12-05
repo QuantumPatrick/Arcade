8adc3dea6153d0c42d9618c78f816166f7c9a6e0
TheRecursiveMind <tehrecursivemind@protonmail.com>
2021-12-05 16:39:23 -0700

    LibraryScreen has buttons

--- ArcadeProject/src/screen/LibraryScreen.java
+++ ArcadeProject/src/screen/LibraryScreen.java
@@ -1,14 +1,35 @@
 package screen;
 
-import java.util.ArrayList;
-
-import games.*;
-import people.*;
+import javax.swing.*;
 import system.*;
-
-import java.util.Scanner;
 
 public class LibraryScreen extends Screen {
 	Library lib = new Library();
+	JFrame frame;
 	
+	LibraryScreen() {
+		frame = new JFrame();
+		
+		JLabel text = new JLabel("Game Library", SwingConstants.CENTER);
+		
+		JButton tttButton = new JButton("TicTacToe");
+		JButton snakeButton = new JButton("Snake");
+		
+		frame.add(text);
+		frame.add(tttButton);
+		frame.add(snakeButton);
+		
+		text.setBounds(200,0,200,50);
+		tttButton.setBounds(150,50,100,50);
+		snakeButton.setBounds(350,50,100,50);
+		
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setSize(600,600);
+		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+	
+	public static void main(String args[]) {
+		new LibraryScreen();
+	}
 }
