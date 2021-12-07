1764d4a3cc35f07494c776a4011544676a30e223
HaseebIrfan <55339617+HaseebIrfan@users.noreply.github.com>
2021-12-06 18:58:30 -0700

    added driver

---
+++ ArcadeProject/src/system/Driver.java
@@ -0,0 +1,14 @@
+package system;
+
+import screen.Screen;
+import screen.SignInScreen;
+
+public class Driver {
+	
+	public static void main(String args[]) {
+		Arcade arcade = new Arcade();
+		Screen SignInScreen = new SignInScreen();
+	}
+	
+	
+}
