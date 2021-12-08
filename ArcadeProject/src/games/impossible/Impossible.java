643ab734fd2eb8cc802816b8be5813c3064b4ef0
HaseebIrfan <55339617+HaseebIrfan@users.noreply.github.com>
2021-12-07 22:25:01 -0700

    started new game

--- ArcadeProject/src/games/impossible/Impossible.java
+++ ArcadeProject/src/games/impossible/Impossible.java
@@ -1,7 +1,14 @@
 package games.impossible;
 
+import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 
@@ -10,12 +17,105 @@ import games.Game;
 public class Impossible extends Game {
 
 	JFrame frame;
+	Integer finalScore;
+	Integer position;
+	boolean running;
 	
 	public Impossible() {
+		position = 300;
 		JFrame frame = new JFrame();
-		//super.paintComponent(g);
-		
+		ScreenCanvas sc = new ScreenCanvas();
+		frame.add(sc);
+		frame.setVisible(true);
 		frame.repaint();
+		frame.setSize(600,600);
+		frame.setBackground(Color.blue);
+		running = true;
+	}
+	
+	public void end() {
+		
+		
+		
+		frame.removeAll();
+		
+	}
+	
+	public class ScreenCanvas extends Canvas implements KeyListener {
+	
+		public ScreenCanvas(){
+			this.addKeyListener(this);
+		}
+
+		//set up a function to draw the shapes
+	    @Override
+	    public void paint (Graphics graphics){
+	    	graphics.setColor(Color.blue.cyan); 
+	    	graphics.fillRect(0,0,600,400);
+	    	graphics.setColor(Color.green); 
+	    	graphics.fillRect(0,400,600,200);
+	    	graphics.setColor(Color.red); 
+	        graphics.fillRect(100, position, 20, 20);
+	    }
+	    
+	    public void endGame() {
+	    	running = false;
+	    }
+	    
+	    public void update() {
+	    	repaint();
+	    }
+	    
+	    public void moveUp() {
+	    	if (running) {
+		    	position -= 10;
+		    	}
+	    	if (position <= 0) {
+	    		endGame();
+	    	}
+	    	//System.out.println("up");
+	    }
+	    
+	    public void moveDown() {
+	    	if (running) {
+	    	position += 10;
+	    	}
+	    	if (position >= 540) {
+	    		endGame();
+	    	}
+	    	//System.out.println("down");
+	    }
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			// TODO Auto-generated method stub
+			if (e.getKeyCode() == KeyEvent.VK_UP) {
+				moveUp();
+			}
+			System.out.println("recognized");
+			if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+				moveDown();
+			} 
+			//System.out.println("recognized");
+			update();
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+	
 	}
 	
 }
+
+
+
+
---
+++ ArcadeProject/src/games/impossible/ImpossibleDriver.java
@@ -0,0 +1,7 @@
+package games.impossible;
+
+public class ImpossibleDriver {
+	public static void main(String[] args) {
+		Impossible game = new Impossible();
+	}
+}
