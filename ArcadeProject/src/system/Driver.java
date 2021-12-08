0656bad32eeca897c5339e1bf6f47b690bc3fbff
TheRecursiveMind <77089965+QuantumPatrick@users.noreply.github.com>
2021-12-08 01:12:50 -0700

    Adds close confirmation

--- ArcadeProject/src/system/Driver.java
+++ ArcadeProject/src/system/Driver.java
@@ -1,24 +1,33 @@
 package system;
 
-import javax.swing.JFrame;
-import javax.swing.WindowConstants;
+import javax.swing.*;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowAdapter;
 
 import screen.Screen;
-import screen.SignInScreen;
 
 public class Driver {
 	
 	public static void main(String args[]) {
 		Leaderboard lbTTT = new Leaderboard("TicTacToe");
 		Leaderboard lbSnake = new Leaderboard("Snake");
+		
 		Arcade arcade = new Arcade(lbTTT, lbSnake);
+		arcade = Arcade.loadArcadeState();
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.setSize(600,600);
 		frame.setVisible(true);
 		Screen mainScreen = new Screen(frame);
 		mainScreen.goToSignIn();
+		
+		
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				Arcade.saveArcadeState(arcade);
+				JOptionPane.showConfirmDialog(null, "Are you sure you want to quit?");
+			}
+		});
 	}
-	
-	
 }
